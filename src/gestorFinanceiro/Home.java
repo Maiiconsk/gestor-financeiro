5a7b19573898eb802e89f3a21833a0fc7531006a
Maiiconsk <Maiiconsk@gmail.com>
2026-08-12 23:10:44 -0300

    Criação do Menu principal

--- src/gestorFinanceiro/Home.java
+++ src/gestorFinanceiro/Home.java
@@ -5,11 +5,46 @@ import java.util.Scanner;
 public class Home {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Hello World!");
-		
-		
+		Scanner sc = new Scanner(System.in);
+
+		boolean run = true;
+
+		while (run) {
+
+			int option = -1;
+
+			System.out.println("======== Controle de Gastos ========");
+			System.out.println("Digite (1) Adicionar uma Transação");
+			System.out.println("Digite (2) Listar suas Transação");
+			System.out.println("Digite (3) Ver Saldo");
+			System.out.println("Digite (4) Fechar o programa");
+			option = sc.nextInt();
+
+			switch (option) {
+
+			case 1:
+				System.out.println("Adicionando Transações...");
+				break;
+			case 2:
+				System.out.println("Listando Transações...");
+				break;
+			case 3:
+				System.out.println("Saldo atual: xxxx");
+				break;
+			case 4:
+				System.out.println("Fechando o programa...");
+
+				run = false;
+
+				break;
+			default:
+				System.out.println("Opção inválida!");
+				break;
+
+			}
+
+		}
+
 		sc.close();
 	}
 
