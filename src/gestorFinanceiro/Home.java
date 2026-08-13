4f6d001b1647ff0a40bab270c51c844b58525bad
Maiiconsk <Maiiconsk@gmail.com>
2026-08-12 22:21:41 -0300

    Criando a estrutura inicial do projeto - Hello World

---
+++ src/gestorFinanceiro/Home.java
@@ -0,0 +1,16 @@
+package gestorFinanceiro;
+
+import java.util.Scanner;
+
+public class Home {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Hello World!");
+		
+		
+		sc.close();
+	}
+
+}
